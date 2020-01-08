0793407deb125a2b2c3b9e1332d866b733b5156a
Stefan Wraase <dev@stfwrs.de>
2020-01-08 22:07:20 +0100

    Update script for forum imgs tags to python 3

--- assets/posts/create_img_tags_for_forums.py
+++ assets/posts/create_img_tags_for_forums.py
@@ -1,29 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-try:
-    import Tkinter as tk
-except:
-    import tkinter as tk
+import clipboard as cp
 import sys
 
-myprint = sys.stdout.write
 post_path = "http://www.grindlheim.com/assets/posts/"
 
-# Create post
-post_id = input("Post ID:")
-n_imgs = input("Anzahl Bilder:")
+# Ask for post id and number of images
+post_id = int(input("Post ID:"))
+n_imgs = int(input("Anzahl Bilder:"))
 
+# Build the result string
 result = ""
 for i in range(1, n_imgs + 1):
     result += ('\n\n[img]' + post_path + str(post_id) + '/p' + str(post_id) + '-' + str(i) + '.jpg' + '[/img]' + '\n')
-myprint(result)
+sys.stdout.write(result)
 
-# Copy to clipboard
-copy_to_cp = raw_input("In die Zwischenablage kopieren (y/n)?")
+# Copy result to clipboard
+copy_to_cp = str(input("In die Zwischenablage kopieren (y/n)?"))
 if copy_to_cp == 'y':
-    cp = tk.Tk()
-    cp.withdraw()
-    cp.clipboard_clear()
-    cp.clipboard_append(result)
-    cp.update()
-    cp.destroy()
+    cp.copy(result)
